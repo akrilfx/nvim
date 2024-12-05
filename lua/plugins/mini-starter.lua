34a1f5af848b0342692f4b1885a37fe0e7c12c09
akrilfx <akrilax@protonmail.com>
2024-12-05 18:34:13 +0100

    - support for autocompletion

--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -1,7 +1,8 @@
 return {
-    "echasnovski/mini.starter",
-    version = false,
-    config = function()
-        require("mini.starter").setup()
-    end,
+	"echasnovski/mini.starter",
+	enabled = false,
+	version = false,
+	config = function()
+		require("mini.starter").setup()
+	end,
 }
