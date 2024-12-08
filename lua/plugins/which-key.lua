9ce7a1a766c20b9fbe3a524688bca43c319ea20f
akrilfx <akrilax@protonmail.com>
2024-12-08 16:33:03 +0100

    adding git-signs

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,26 +1,43 @@
-return
-{
+return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     lazy = true,
+    cmd = "WhichKey",
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
     opts = {
         preset = "helix",
         delay = 0,
         win = {
             height = {
-              max = math.huge,
+                max = math.huge,
             },
         },
         plugins = {
             spelling = {
-              enabled = false,
+                enabled = true,
             },
         },
         icons = {
             rules = false,
-            breadcrumb = " ", -- symbol used in the command line area that shows your active key combo
-            -- separator = "󱦰  ", -- symbol used between a key and it's label
+            breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+            separator = "󱦰 ", -- symbol used between a key and it's label
             group = "󰹍 ", -- symbol prepended to a group
+        },
+        spec = {
+            -- { "g",         group = "goto" },
+            { "z",         group = "fold" },
+            { "<leader>c", group = "code" },
+            { "<leader>g", group = "git" },
+            { "<leader>q", group = "quit/session" },
+            { "<leader>t", group = "telescope" },
         },
     },
 }
